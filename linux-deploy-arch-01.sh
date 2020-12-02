778524c88e4c89730e90ff9771c5fa6c92304ec1
Sepero <sepero@gmx.com>
2020-12-02 16:31:19 +0700

    Prep main script for testing

--- linux-deploy-arch-01.sh
+++ linux-deploy-arch-01.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-#sudo  pacman -S --noconfirm --overwrite '*' base-devel go git x11vnc
-# download + build PKGBUILD for temp fakeroor-tcp
+sudo pacman -S --noconfirm --overwrite '*' base-devel go git x11vnc
+# download + build PKGBUILD for temp fakeroot-tcp
 
-export PATH=/opt/fakeroot/bin:$PATH
+# export PATH=/opt/fakeroot/bin:$PATH
 
-git clone https://aur.archlinux.org/fakeroot-tcp.git
+# git clone https://aur.archlinux.org/fakeroot-tcp.git
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
 
 # Set user default shell to bash
 # chsh -s /bin/bash a
