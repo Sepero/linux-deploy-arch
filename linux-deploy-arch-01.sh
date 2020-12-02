8ca0593160f817b22729e04894916703afc1d625
Sepero <sepero111@gmx.com>
2020-12-02 22:46:20 +0700

    Lots of prep

--- linux-deploy-arch-01.sh
+++ linux-deploy-arch-01.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --overwrite '*' base-devel go git x11vnc
+sudo pacman -S --noconfirm --overwrite '*' base-devel go git #x11vnc
 # download + build PKGBUILD for temp fakeroot-tcp
+
+sudo chsh -s /bin/bash $(whoami)
+
+# Clone full installer.
+git clone https://github.com/Sepero/linux-deploy-arch
 
 # export PATH=/opt/fakeroot/bin:$PATH
 
 # git clone https://aur.archlinux.org/fakeroot-tcp.git
 
-# git clone https://aur.archlinux.org/yay.git
+git clone https://aur.archlinux.org/yay.git
 # cd yay
 # makepkg -si --noconfirm
 
@@ -17,5 +22,9 @@ sudo pacman -S --noconfirm --overwrite '*' base-devel go git x11vnc
 #vncserver :0
 
 # Bash recommended
-# export HISTSIZE=3000
-# export HISTCONTROL=ignoreboth
+echo 'export HISTSIZE=3000' >> .bashrc
+echo 'export HISTCONTROL=ignoreboth' >> .bashrc
+echo 'export LD_LIBRARY_PATH=$HOME/fr/libs:/lib:/usr/lib' >> .bashrc
+echo 'export PATH=$HOME/fr/fakeroot/bin:$PATH' >> .bashrc
+#export PATH=/opt/fakeroot/bin:$PATH' >> .bashrc
+#export LD_LIBRARY_PATH=/opt/fakeroot/libs:/lib:/usr/lib' >> .bashrc
